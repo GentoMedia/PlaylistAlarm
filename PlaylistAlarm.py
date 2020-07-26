a20d00092ce586283a311c02f0b4db7ba11d9485
Darien Sebastion Charles <dariencharles@gmail.com>
2020-07-26 12:05:46 +0900

    Added URL argument
    Added a second argument to allow the input of a url.

--- PlaylistAlarm.py
+++ PlaylistAlarm.py
@@ -1,17 +1,20 @@
 import webbrowser
 import datetime
+import re
 from time import sleep
 from sys import argv
 
 alarm_time = datetime.timedelta()
 alarm_hour = 0
-alarm_minute = 0	
+alarm_minute = 0
+url = "https://www.youtube.com/watch?v=lgtoypeY9yg&list=PLLU7Oh2-moMV2MEeCt6GgXSCCaSlP3LIR"
 
+#-Checking for wakeup time or asking for it----------------------
 if len(argv) > 1:
 	intime = datetime.datetime.strptime(str(argv[1]), "%H%M").time()
 	alarm_hour = intime.hour
 	alarm_minute = intime.minute
-	
+
 else:
 	print("What time do you want to wake up?\n")
 
@@ -20,6 +23,19 @@ else:
 
 	print("\nEnter minutes(0-59)")
 	alarm_minute = int(input(">>> "))
+#----------------------------------------------------------------
+
+#-Checking if there is a URL or using the default----------------
+if len(argv) > 2:
+		url = str(argv[2])
+#----------------------------------------------------------------
+
+#-Enabling autoplay if not set-----------------------------------
+if re.match(r"&autoplay=1", url):
+	pass
+else:
+	url += "&autoplay=1"
+#----------------------------------------------------------------
 
 
 ntime = datetime.datetime.now()
@@ -48,4 +64,4 @@ print("    ", sleep_time)
 
 sleep(sleep_time.total_seconds())
 
-webbrowser.open("https://www.youtube.com/embed/videoseries?list=PLLU7Oh2-moMV2MEeCt6GgXSCCaSlP3LIR&autoplay=1", new=0)
+webbrowser.open(url, new=0)
